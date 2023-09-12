27d1ecdb1325263ea379d00d66f3095db662f230
안찬영 <chan20172@gmail.com>
2023-09-12 14:24:35 +0900

    TODO리스트 기능 보완

--- src/App.js
+++ src/App.js
@@ -1,27 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
-  const taskList = props.tasks.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if(id === task.id){
+        return { ...task, completed: !tasks.completed };
+      }
+
+      return task;
+    });
+    setTasks(updatedTasks);    
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editTaskList = tasks.map((task) => {
+      if(id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editTaskList)
+  }
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  const headingText = `${taskList.length}개의 작업이 남음`;
+
+  function addTask(name) {
+    const newTask = {id: `todo-${nanoid()}`, name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+      {filterList}
       </div>
-      <h2 id="list-heading">3개의 작업이 남음</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,23 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo(props) {
-    return (
-      <li className="todo stack-small">
-        <div className="c-cb">
-          <input id="todo-0" type="checkbox" defaultChecked={props.completed} />
-          <label className="todo-label" htmlFor={props.id}>
-            {props.name}
-          </label>
-        </div>
-        <div className="btn-group">
-          <button type="button" className="btn">
-            Edit <span className="visually-hidden">{props.name}</span>
-          </button>
-          <button type="button" className="btn btn__danger">
-            Delete <span className="visually-hidden">{props.name}</span>
-          </button>
-        </div>
-      </li>
-    );
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState("");
+
+  function handleChange(e) {
+    setNewName(e.target.value);
   }
-  
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.editTask(props.id, newName);
+    setNewName("");
+    setEditing(false);
+  }
+
+  const editingTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="todo-label" htmlFor={props.id}>
+          {props.name}의 새로운 이름
+        </label>
+        <input 
+          id={props.id} 
+          className="todo-text" 
+          type="text"
+          value={newName}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="btn-group">
+        <button 
+          type="button" 
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}>
+          취소
+          <span className="visually-hidden">{props.name} 이름 바꾸기</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          저장
+          <span className="visually-hidden">{props.name}의 새로운 이름</span>
+        </button>
+      </div>
+    </form>
+  );
+
+  const viewTemplate = (
+    <div className="stack-small">
+      <div className="c-cb">
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
+        <label className="todo-label" htmlFor={props.id}>
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button 
+          type="button" 
+          className="btn"
+          onClick={() => setEditing(true)}>
+          편집 <span className="visually-hidden">{props.name}</span>
+        </button>
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => props.deleteTask(props.id)}>
+          삭제 <span className="visually-hidden">{props.name}</span>
+        </button>
+      </div>
+    </div>
+  );  
+
+  return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
+}
